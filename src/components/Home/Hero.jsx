b042ad8c8c0a4c0f25214841b956c7db7c089a2f
Mallesh Pareet <malleshpareet360@gmail.com>
2026-02-20 13:14:11 +0530

    Bold and Color usage

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,13 +3,14 @@ import BannerBox from '../../assets/images/Homepage/Homepage Banner.svg';
 
 const Hero = () => {
     return (
-        <section className="hero-section">
+        <section className="hero-section min-h-[50vh] md:min-h-[70vh] flex items-center">
             <img
                 src={BannerBox}
                 alt="The Starry Path Banner"
                 className="hero-banner"
                 loading="eager"
                 fetchpriority="high"
+
             />
         </section>
     );
